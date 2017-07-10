792f99af4a2d620a078cdb6ad1585f58734e3d44
Tom Wolfe <trwolfe13@gmail.com>
2017-07-10 16:43:08 +0100

    Add another dice roll parse test

--- spec/parser/dice-parser.parseDiceRoll.spec.ts
+++ spec/parser/dice-parser.parseDiceRoll.spec.ts
@@ -21,21 +21,21 @@ describe("DiceParser", () => {
             expect(dice.getChild(1).type).toBe(NodeType.DiceSides);
             expect(dice.getChild(1).getAttribute("value")).toBe(6);
         });
-        it("can correctly parse a simple dice roll.", () => {
+        it("can correctly parse a simple dice roll with modifier (10d6dl3).", () => {
             const lexer = new MockLexer([
                 new Token(TokenType.Integer, 0, "10"),
                 new Token(TokenType.Identifier, 2, "d"),
-                new Token(TokenType.Integer, 3, "6")
+                new Token(TokenType.Integer, 3, "6"),
+                new Token(TokenType.Identifier, 4, "dl"),
+                new Token(TokenType.Integer, 5, "3"),
             ]);
             const parser = new Parser.DiceParser(lexer);
             const dice = parser.parseDiceRoll();
-            expect(dice.type).toBe(NodeType.Dice);
+            expect(dice.type).toBe(NodeType.Drop);
+            expect(dice.getAttribute("type")).toBe("lowest");
             expect(dice.getChildCount()).toBe(2);
-            expect(dice.getChild(0).type).toBe(NodeType.Integer);
-            expect(dice.getChild(0).getAttribute("value")).toBe(10);
-            expect(dice.getChild(1).type).toBe(NodeType.DiceSides);
-            expect(dice.getChild(1).getAttribute("value")).toBe(6);
+            expect(dice.getChild(0).type).toBe(NodeType.Dice);
+            expect(dice.getChild(1).type).toBe(NodeType.Integer);
         });
-        // TODO: Check modifiers
     });
 });
