e8b62d1f1b2a4a08e50480af17a74b75c7df70f4
Tom Wolfe <trwolfe13@gmail.com>
2017-07-24 08:23:31 +0100

    Fix redundant test.

--- spec/generator/dice-generator.generate.throws.ts
+++ spec/generator/dice-generator.generate.throws.ts
@@ -3,34 +3,16 @@ import * as Generator from "../../src/generator";
 
 describe("DiceGenerator", () => {
     describe("generate", () => {
-        it("generates a simple critical success (2d6cs).", () => {
-            const exp = Ast.Factory.create(Ast.NodeType.Critical)
-                .setAttribute("type", "success");
-
-            const dice = Ast.Factory.create(Ast.NodeType.Dice);
-            dice.addChild(Ast.Factory.create(Ast.NodeType.Integer).setAttribute("value", 2));
-            dice.addChild(Ast.Factory.create(Ast.NodeType.DiceSides).setAttribute("value", 6));
-
-            exp.addChild(dice);
-
-            const generator = new Generator.DiceGenerator();
-            expect(generator.generate(exp)).toBe("2d6cs");
-        });
-        it("generates a critical success (2d6cs=6).", () => {
-            const exp = Ast.Factory.create(Ast.NodeType.Critical)
-                .setAttribute("type", "success");
+        it("throws on unrecognized node type (--).", () => {
+            const exp = new Ast.ExpressionNode(<any>"Face", null);
 
             const dice = Ast.Factory.create(Ast.NodeType.Dice);
             dice.addChild(Ast.Factory.create(Ast.NodeType.Integer).setAttribute("value", 2));
             dice.addChild(Ast.Factory.create(Ast.NodeType.DiceSides).setAttribute("value", 6));
             exp.addChild(dice);
 
-            const eq = Ast.Factory.create(Ast.NodeType.Equal);
-            eq.addChild(Ast.Factory.create(Ast.NodeType.Integer).setAttribute("value", 6));
-            exp.addChild(eq);
-
             const generator = new Generator.DiceGenerator();
-            expect(generator.generate(exp)).toBe("2d6cs=6");
+            expect(() => generator.generate(exp)).toThrow();
         });
     });
 });
