ebab11566fba17c61e038a1c9815f0d5dbf08bbd
Tom Wolfe <TOM356@bailliegifford.com>
2018-10-10 09:12:59 +0100

    Add keep/drop count to rendered expression output.

--- spec/generator/dice-generator.generate.drop.spec.ts
+++ spec/generator/dice-generator.generate.drop.spec.ts
@@ -29,5 +29,19 @@ describe('DiceGenerator', () => {
       const generator = new Generator.DiceGenerator();
       expect(generator.generate(exp)).toBe('2d6dh');
     });
+    it('generates a drop with modifier (3d6dh2).', () => {
+      const exp = Ast.Factory.create(Ast.NodeType.Drop)
+        .setAttribute('type', 'highest');
+
+      const dice = Ast.Factory.create(Ast.NodeType.Dice);
+      dice.addChild(Ast.Factory.create(Ast.NodeType.Number).setAttribute('value', 3));
+      dice.addChild(Ast.Factory.create(Ast.NodeType.DiceSides).setAttribute('value', 6));
+
+      exp.addChild(dice);
+      exp.addChild(Ast.Factory.create(Ast.NodeType.Number).setAttribute('value', 2));
+
+      const generator = new Generator.DiceGenerator();
+      expect(generator.generate(exp)).toBe('3d6dh2');
+    });
   });
 });
